85c0768c5d577ec7949cad2990683d6b8d2eb65c
morozov1van <m0r0z0ff1van1@gmail.com>
2020-05-26 21:02:51 +0300

    start django app

--- bfs/urls.py
+++ bfs/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bfs_app.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', index),
 ]
