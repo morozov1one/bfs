a27e96d94cfd708c14af7d006ec0a6f780a6e8a7
m0r0zk01 <m0r0z0ff1van1@gmail.com>
2020-06-02 14:49:44 +0300

    second thread for infinite loop works(I think so)

--- bfs/settings.py
+++ bfs/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '(m1&x2+(z$sj0$#xg(f87idcww8@@udwqs6*y10!it^q-3w+fx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
--- manage.py
+++ manage.py
@@ -2,72 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from threading import Thread
-from time import time, sleep
-from web3 import Web3, HTTPProvider, eth
-import requests
-import json
-
-
-def get_usd_eth():
-    r = requests.get('https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=ETH')
-    return json.loads(r.text)['ETH']
-
-
-class UpdateSmartContract(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        with open('solidity/infura_api_token', 'r') as file:
-            text = file.read()
-            os.environ['WEB3_INFURA_PROJECT_ID'] = text
-            self.w3 = Web3(HTTPProvider('https://goerli.infura.io/v3/' + text))
-        self.prices = {
-            0: 1,
-            1: 10,
-            2: 50,
-            3: 2,
-        }
-
-    def update(self):
-        with open('solidity/private_key', 'r') as file:
-            private_key = file.read()
-        with open('solidity/abi', 'r') as file:
-            abi = file.read()
-        contract_address = '0x3b1C4370D52692dFfbe0cFC9C2cc0935b0d0f747'
-        self.w3.eth.account = '0x0da52A47b11fFFefEf609E41FCF956b52ca9a2Ef'
-        tmp_contract = self.w3.eth.contract(address=contract_address, abi=abi)
-        eth.Account.privateKeyToAccount(private_key=private_key)
-        for account in range(4):
-            wei = int(self.prices[account] * get_usd_eth() * 10**18)
-            txn = tmp_contract.functions.setPrice(account, wei).buildTransaction(
-                {
-                    'chainId': 5,
-                    'gas': 300000,
-                    'gasPrice': self.w3.eth.gasPrice,
-                    'nonce': self.w3.eth.getTransactionCount(self.w3.eth.account),
-                    'value': 0,
-                }
-            )
-            signed_txn = eth.Account.sign_transaction(txn, private_key)
-            self.w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-
-    def run(self):
-        next_time = time()
-        while True:
-            self.update()
-            next_time += 12 * 60 * 60
-            sleep_time = next_time - time()
-            if sleep_time > 0:
-                sleep(sleep_time)
+from start_thread import start_thread
 
 
 def main():
-    """
-        Start infinite loop in second thread for updating Admin smart-contract every 12 hours
-    """
-    thread = UpdateSmartContract()
-    thread.start()
-
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bfs.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -81,4 +19,6 @@ def main():
 
 
 if __name__ == '__main__':
+    start_thread()
     main()
+
