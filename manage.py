804f56f624f51b2298f92a1735160143bfe3055b
m0r0zk01 <m0r0z0ff1van1@gmail.com>
2020-06-02 14:15:57 +0300

    merging

--- manage.py
+++ manage.py
@@ -16,28 +16,32 @@ def get_usd_eth():
 
 class UpdateSmartContract(Thread):
     def __init__(self):
-        with open('infura_api_token', 'r') as file:
-            self.w3 = Web3(HTTPProvider('https://ropsten.infura.io/v3/' + file.read()))
-        self.prices = {
-            1: 1,
-            2: 10,
-            3: 50,
-            4: 2,
-        }
         Thread.__init__(self)
+        with open('solidity/infura_api_token', 'r') as file:
+            text = file.read()
+            os.environ['WEB3_INFURA_PROJECT_ID'] = text
+            self.w3 = Web3(HTTPProvider('https://goerli.infura.io/v3/' + text))
+        self.prices = {
+            0: 1,
+            1: 10,
+            2: 50,
+            3: 2,
+        }
 
     def update(self):
-        with open('private_key', 'r') as file:
+        with open('solidity/private_key', 'r') as file:
             private_key = file.read()
-        abi = ''
-        contract_address = ''
+        with open('solidity/abi', 'r') as file:
+            abi = file.read()
+        contract_address = '0x3b1C4370D52692dFfbe0cFC9C2cc0935b0d0f747'
         self.w3.eth.account = '0x0da52A47b11fFFefEf609E41FCF956b52ca9a2Ef'
         tmp_contract = self.w3.eth.contract(address=contract_address, abi=abi)
+        eth.Account.privateKeyToAccount(private_key=private_key)
         for account in range(4):
-            wei = self.prices[account] * get_usd_eth() / 10**18
+            wei = int(self.prices[account] * get_usd_eth() * 10**18)
             txn = tmp_contract.functions.setPrice(account, wei).buildTransaction(
                 {
-                    'chainId': 3,
+                    'chainId': 5,
                     'gas': 300000,
                     'gasPrice': self.w3.eth.gasPrice,
                     'nonce': self.w3.eth.getTransactionCount(self.w3.eth.account),
