acb0332fcd2b94bb7ddf8f26858043c8aa9388d5
Olek_Olegovich <olek.olegovich@gmail.com>
2020-06-04 19:34:53 +0300

    helping_users

--- solidity/bfs_contracts.sol
+++ solidity/bfs_contracts.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.6.0 <= 0.6.8;
 
-import 'https://github.com/morozov1one/bfs/blob/master/solidity/Admin.sol';
+import 'https://github.com/morozov1one/bfs/blob/dev_sol/smart_contracts/Admin.sol';
 
 contract Transfer_money {
     function send(address payable recipient) external payable {
@@ -12,36 +12,36 @@ contract Main {
     address private owner;
     address payable constant bfs_wallet = 0x0da52A47b11fFFefEf609E41FCF956b52ca9a2Ef;
     address constant admin_address = 0x3b1C4370D52692dFfbe0cFC9C2cc0935b0d0f747;
-
-    uint64[4] private subs_days; // сколько дней подписки осталось
-    uint256[4] private last_upd; // последнее обновление
-
-    User user;
-    Banker banker;
-    Business business;
-    Investor investor;
-    Transfer_money tm;
-
+    
+    uint64[4] private subs_days; //how many subscription days are left
+    uint256[4] private last_upd; //latest update
+    
+    User private user;
+    Banker private banker;
+    Business private business;
+    Investor private investor;
+    Transfer_money private tm;
+    
     event ConstructorInitiated(string nextStep);
     event Deposit(address _sender, uint amount);
 	event Withdraw(address _sender, uint amount, address recipient);
-
+    
     constructor () public {
-        emit ConstructorInitiated("Вызван конструктор Main");
+        emit ConstructorInitiated("constructor Main");
         Admin admin = Admin(admin_address);
-        require(admin.checkUser(msg.sender));
+        require(!admin.checkUser(msg.sender));
         owner = msg.sender;
-        subs_days[0] = 0; //Обычный пользователь
-        subs_days[1] = 0; //Банкир
-        subs_days[2] = 0; //Предприятие
-        subs_days[3] = 0; //Инвестор
-        last_upd[0] = now; //Обычный пользователь
-        last_upd[1] = now; //Банкир
-        last_upd[2] = now; //Предприятие
-        last_upd[3] = now; //Инвестор
+        subs_days[0] = 0;
+        subs_days[1] = 0;
+        subs_days[2] = 0;
+        subs_days[3] = 0;
+        last_upd[0] = now;
+        last_upd[1] = now;
+        last_upd[2] = now;
+        last_upd[3] = now;
         tm = new Transfer_money();
     }
-
+    
     function createProfiles() public {
         require(msg.sender == owner);
         user = new User(address(this));
@@ -49,31 +49,31 @@ contract Main {
         business = new Business(address(this));
         investor = new Investor(address(this));
     }
-
+    
     function getOwner() external view returns (address) {
         return owner;
     }
-
+    
     function getAddress() external view returns(address) {
         return address(this);
     }
-
+    
     function getUserAddress() external view returns(address) {
         return address(user);
     }
-
+    
     function getBankerAddress() external view returns(address) {
         return address(banker);
     }
-
+    
     function getBusinessAddress() external view returns(address) {
         return address(business);
     }
-
+    
     function getInvestorAddress() external view returns(address) {
         return address(investor);
     }
-
+    
     function getDays(uint8 profile) public view returns(uint64) {
         return subs_days[profile];
     }
@@ -100,36 +100,42 @@ contract User {
     address private owner;
     address main_address;
     Transfer_money tm;
-
+    
     constructor (address main) public {
         owner = msg.sender;
         tm = new Transfer_money();
         main_address = main;
     }
-
+    
     function checkSubs(address user, uint8 profile) public returns(bool) {
         Main u = Main(user);
         u.delDays(profile);
         return (u.getDays(profile) > 0);
     }
-
+    
     function sendMoney(address payable recipient) public payable {
         require(checkSubs(main_address, 0));
         tm.send(recipient);
     }
-
+    
     function setDeposit(address banker) public payable {
         require(checkSubs(main_address, 0));
         Banker b = Banker(banker);
         b.setDeposit(main_address);
     }
-
+    
+    function getMoneyFromDeposit(address banker) public payable {
+        require(checkSubs(main_address, 0));
+        Banker b = Banker(banker);
+        b.getMoneyFromDeposit(main_address);
+    }
+    
     function askCredit(address banker, uint256 value, uint8 percent, uint64 term) public {
         require(checkSubs(main_address, 0));
         Banker b = Banker(banker);
         b.askCredit(main_address, value, percent, term);
     }
-
+    
     function payCredit(address banker) external payable {
         require(checkSubs(main_address, 0));
         Banker b = Banker(banker);
@@ -142,39 +148,40 @@ contract Banker {
     address constant admin_address = 0x3b1C4370D52692dFfbe0cFC9C2cc0935b0d0f747;
     address main_address;
     Transfer_money tm;
-
+    
     struct debt {
-        uint256 value; //Значение долга
-        uint8 percent; //"Штрафные" проценты
-        uint64 term; //За сколько дней отдать нужно
-        uint256 last_pay; //Когда платил в последний раз
+        uint256 value;
+        uint8 percent;
+        uint64 term;
+        uint256 last_pay;
     }
-
+    
     struct deposit {
         bool open;
         uint256 value;
         uint8 percent;
-        uint16 term; //Раз в сколько дней начисляются проценты
-        uint256 last_upd; //Когда последний раз начислялись проценты
+        uint16 term;
+        uint256 last_upd;
     }
-
+    
     address[] private list_of_credits;
     mapping(address => deposit) private deposits;
     mapping(address => debt) private credits;
     mapping(address => debt) private ask_credits;
-
+    address payable[] private list_of_deposits;
+    
     constructor (address main) public {
         owner = msg.sender;
         tm = new Transfer_money();
         main_address = main;
     }
-
+    
     function checkSubs(address user, uint8 profile) public returns(bool) {
         Main u = Main(user);
         u.delDays(profile);
         return (u.getDays(profile) > 0);
     }
-
+    
     function getDeposit(address client, uint8 percent, uint16 term) public {
         require(msg.sender == owner);
         require(checkSubs(main_address, 1));
@@ -184,7 +191,7 @@ contract Banker {
         deposits[client].percent = percent;
         deposits[client].term = term;
     }
-
+    
     function getPercents() public {
         require(deposits[msg.sender].open == true);
         uint16 count = uint16((now - deposits[msg.sender].last_upd) / 86400 / deposits[msg.sender].term);
@@ -192,24 +199,49 @@ contract Banker {
             deposits[msg.sender].value += (deposits[msg.sender].value * deposits[msg.sender].percent / 100);
         deposits[msg.sender].last_upd = now;
     }
-
+    
+    function getPercents(address client) public {
+        require(msg.sender == owner);
+        require(deposits[client].open == true);
+        uint16 count = uint16((now - deposits[client].last_upd) / 86400 / deposits[client].term);
+        for (uint16 i = 0; i < count; ++i)
+            deposits[client].value += (deposits[client].value * deposits[client].percent / 100);
+        deposits[client].last_upd = now;
+    }
+    
+    function getMoneyFromDeposit(address client) external {
+        Admin admin = Admin(admin_address);
+        require(!admin.checkUser(msg.sender, client));
+        require(deposits[msg.sender].open == true);
+        list_of_deposits.push(msg.sender);
+    }
+    
+    function returnMoney() public payable {
+        require(msg.sender == owner);
+        for (uint16 i = 0; i < list_of_deposits.length; ++i) {
+            getPercents(address(list_of_deposits[i]));
+            tm.send(list_of_deposits[i]);
+        }
+        list_of_deposits = new address payable[](0);
+    }
+    
     function setDeposit(address user) external payable {
         Admin admin = Admin(admin_address);
-        require(admin.checkUser(msg.sender, user));
+        require(!admin.checkUser(msg.sender, user));
         require(deposits[msg.sender].open == true);
         getPercents();
         deposits[msg.sender].value += msg.value;
     }
-
+    
     function askCredit(address user, uint256 value, uint8 percent, uint64 term) external {
         Admin admin = Admin(admin_address);
-        require(admin.checkUser(msg.sender, user));
+        require(!admin.checkUser(msg.sender, user));
         require(credits[msg.sender].value == 0);
         ask_credits[msg.sender].value = value;
         ask_credits[msg.sender].percent = percent;
         ask_credits[msg.sender].term = term;
     }
-
+    
     function getCredit(address client, uint256 value, uint8 percent, uint64 term) public {
         require(msg.sender == owner);
         require(checkSubs(main_address, 1));
@@ -222,7 +254,7 @@ contract Banker {
         credits[client].last_pay = now;
         list_of_credits.push(client);
     }
-
+    
     function checkOneCredit(address client) public {
         require(credits[client].value > 0);
         uint16 count = uint16((now - credits[client].last_pay) / 86400 / credits[client].term);
@@ -230,17 +262,17 @@ contract Banker {
             credits[client].value += (credits[client].value * credits[client].percent / 100);
         credits[client].last_pay = now;
     }
-
+    
     function checkCredits() public {
         require(msg.sender == owner);
         require(checkSubs(main_address, 1));
         for (uint16 i = 0; i < list_of_credits.length; ++i)
             checkOneCredit(list_of_credits[i]);
     }
-
+    
     function payCredit(address user) external payable {
         Admin admin = Admin(admin_address);
-        require(admin.checkUser(msg.sender, user));
+        require(!admin.checkUser(msg.sender, user));
         checkOneCredit(msg.sender);
         require(msg.value >= credits[msg.sender].value);
         credits[msg.sender].value = 0;
@@ -249,19 +281,19 @@ contract Banker {
 
 
 /*
-Реализацию контрактов для бизнеса и инвесторов
-невозможно написать в рамках учебного проекта,
-так как мы не можем сделать выпуск ценных бумаг.
-
-Этот функционал можно воспринимать как
-перспективы развития проекта.
+The implementation of contracts for business
+and investors cannot be written within the 
+framework of a training project, since we 
+cannot issue securities.
+This functionality can be perceived as 
+a project development perspective
 */
 
 contract Business {
     address private owner;
     address main_address;
     Transfer_money tm;
-
+    
     constructor (address main) public {
         owner = msg.sender;
         tm = new Transfer_money();
@@ -273,7 +305,7 @@ contract Investor {
     address private owner;
     address main_address;
     Transfer_money tm;
-
+    
     constructor (address main) public {
         owner = msg.sender;
         tm = new Transfer_money();
